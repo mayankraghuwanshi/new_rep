8aec87cc733ddf8ec6e7170bb7479649930a50a0
mayank raghuvanshi <mayank1272@hotmail.com>
2020-01-27 21:08:45 +0530

    router is not working with gh-og

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route,} from "react-router-dom";
+import {BrowserRouter as Router, HashRouter, Route,} from "react-router-dom";
 
 import SortVisualizer from "./Components/sorting/SortVisualizer";
 import PathfindingVisualizer from "./Components/pathfinding/pathfindingVisualizer";
 function App() {
 
-    return <Router basename={process.env.PUBLIC_URL}>
+    return <HashRouter basename={process.env.PUBLIC_URL}>
                 <div className={"App"}>
                     <Route exact path={'/'} component = {SortVisualizer}/>
                     <Route exact path={'/pathfinding'} component = {PathfindingVisualizer}/>
                 </div>
-            </Router>
+            </HashRouter>
 }
 
 // function App() {
